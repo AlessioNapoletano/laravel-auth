be88a71c4a35cb62dc044cfeb47edc8a723a8be4
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-22 00:48:20 +0100

    create edit file

---
+++ resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+
+@section('title', "Edit '$post->title' Post")
+
+@section('content')
+    <div class="container my-5">
+        @include('admin.posts.partials.error')
+        @include('admin.posts.partials.form', ['method' => 'PUT', 'action' => 'admin.posts.update' , 'parameter' => 'post->id'])
+    </div>
+@endsection
