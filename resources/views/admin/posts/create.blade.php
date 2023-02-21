2895eefb0053fea3e9ba76ddb9b45f463520d79a
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-21 23:47:58 +0100

    Create patials 'form' and iclude this in create file

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -2,34 +2,6 @@
 
 @section('content')
     <div class="container my-5">
-        <form action="{{ route('admin.posts.store') }}" method="POST">
-            @csrf
-            
-            <h4 class="mb-3 fw-bold">
-                {{ Auth::user()->name }}
-            </h4>
-            <form>
-                <div class="mb-3">
-                    <label for="title" class="form-label">Titolo</label>
-                    <input type="text" class="form-control" id="title" name="title">
-                </div>
-
-                <div class="mb-3">
-                    <label for="post_date" class="form-label">Data di pubblicazione</label>
-                    <input type="date" class="form-control" id="post_date" name="post_date">
-                </div>
-
-                <div class="mb-3">
-                    <label for="content" class="form-label">Contenuto del post</label>
-                    <textarea type="text" class="form-control" id="content" name="content">
-
-                    </textarea>
-                </div>
-
-                <div class="mb-3">
-                    <button type="submit" class="btn btn-primary">Crea Post</button>
-                </div>
-            </form>
-        </form>
+        @include('admin.posts.partials.form', ['method' => 'POST', 'action' => 'admin.posts.store'])
     </div>
 @endsection
