6101a8b0691725d23a88ffecb4e3e20c36b64745
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-22 00:11:33 +0100

    add old method in value attribute

--- resources/views/admin/posts/partials/form.blade.php
+++ resources/views/admin/posts/partials/form.blade.php
@@ -7,18 +7,19 @@
     <form>
         <div class="mb-3">
             <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="title" name="title">
+            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
         </div>
 
         <div class="mb-3">
             <label for="post_date" class="form-label">Data di pubblicazione</label>
-            <input type="date" class="form-control" id="post_date" name="post_date">
+            <input type="date" class="form-control" id="post_date" name="post_date" 
+            value="{{ old('post_date') }}" >
         </div>
 
         <div class="mb-3">
             <label for="content" class="form-label">Contenuto del post</label>
             <textarea type="text" class="form-control" id="content" name="content">
-
+                {{ old('content') }}
             </textarea>
         </div>
 
