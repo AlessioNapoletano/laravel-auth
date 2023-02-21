6c5dfeb7de416b7cf31e879008e9e2429f5df23d
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-22 00:48:50 +0100

    fix create file

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -5,6 +5,6 @@
 @section('content')
     <div class="container my-5">
         @include('admin.posts.partials.error')
-        @include('admin.posts.partials.form', ['method' => 'POST', 'action' => 'admin.posts.store'])
+        @include('admin.posts.partials.form', ['method' => 'POST', 'action' => 'admin.posts.store', 'parameter' => ''])
     </div>
 @endsection
