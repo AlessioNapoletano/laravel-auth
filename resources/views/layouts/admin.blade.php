223645ae393aa7bf68536a382b6fee16df61cb43
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-22 13:03:20 +0100

    add in head element, link and script for 'sweetAlert2'

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,6 +19,9 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    <link href='https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.min.css'>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.6/dist/sweetalert2.all.min.js"></script>
+
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
 </head>
@@ -91,6 +94,7 @@
         <main class="">
             @yield('content')
         </main>
+        @yield('script')
     </div>
 </body>
 
