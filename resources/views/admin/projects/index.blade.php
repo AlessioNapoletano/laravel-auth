dc26aef883e4847054d6be45efb3227520a47124
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-22 13:38:04 +0100

    popolate projects.index page

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,7 +3,57 @@
 @section('title', 'Projects')
 
 @section('content')
-    <h1>
-        Pagina per i progetti
-    </h1>
+<div class="container my-4">
+    <!--Print message-->
+    @if (session('message'))
+        <div class="alert alert-{{session('message-class')}} mb-3">
+            {{ session('message') }}
+        </div>
+    @endif
+    
+    @if (session('alert-message'))
+        <div id="popup_message" class="d-none" data-type="{{ session('alert-type') }}" data-message="{{ session('alert-message') }}"></div>
+    @endif
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Author</th>
+                <th scope="col">Post Date</th>
+                <th scope="col">
+                    <a href="" class="btn btn-primary px-5">Create new project</a>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+            <tr>
+                <td>{{ $project->id }}</td>
+                <td>{{ $project->title }}</td>
+                <td>{{ $project->author }}</td>
+                <td>{{ $project->post_date }}</td>
+                <td>
+                    <a href="" class="btn btn-primary">Show</a>
+                    <a href="" class="btn btn-success">Edit</a>
+
+                    <form class="d-inline delete" action="" method="POST">
+                    @csrf
+                    @method('DELETE')
+
+                    <button class="btn btn-danger">
+                        Delete
+                    </button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+      </table>
+</div>
+@endsection
+
+@section('script')
+    @vite('resources/js/confirmDelete.js')
 @endsection
