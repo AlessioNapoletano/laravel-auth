060ba4f9549fcabf0c1131def6476a3d62d06a2d
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-22 01:55:06 +0100

    add logo of the Post Site

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -27,8 +27,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex align-items-center" href="{{ route('welcome') }}">
+                    <img width="30px"src="https://cdn-icons-png.flaticon.com/512/889/889669.png" alt="Post's logo">
                 </a>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
