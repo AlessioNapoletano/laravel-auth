5a1506ddc943bfc12039307987508f22c0ba41bb
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-22 13:46:56 +0100

    create 'create' page in 'projects' directory

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+
+@section('title', 'Create new Project')
+
+@section('content')
+    <div class="container my-5">
+        @include('admin.projects.partials.error')
+        @include('admin.projects.partials.form', ['method' => 'POST', 'action' => 'admin.projects.store', 'parameter' => '', 'buttonClass' => 'primary', 'buttonText' => 'Crea Post'])
+    </div>
+@endsection
