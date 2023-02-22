270f474eb9b1ba397e45cd5db7ad9dfd9881c40b
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-22 11:27:12 +0100

    add table in file 'trashed'

--- resources/views/admin/posts/trashed.blade.php
+++ resources/views/admin/posts/trashed.blade.php
@@ -3,8 +3,37 @@
 @section('title', 'Post trashed')
 
 @section('content')
-<h1>
-    post eliminati non definitivamente
-</h1>
 
+<section class="trashed-post">
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#id</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Author</th>
+                    <th scope="col">Post Date</th>
+                    <th scope="col">Delete at</th>
+                    <th scope="col">
+                        <a href="" class="btn btn-primary px-5">Ripristina tutti i post</a>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($postsTrashed as $post)
+                <tr>
+                    <td>{{ $post->id }}</td>
+                    <td>{{ $post->title }}</td>
+                    <td>{{ $post->author }}</td>
+                    <td>{{ $post->post_date }}</td>
+                    <td>{{ $post->deleted_at }}</td>
+                    <td>
+                        <a href="" class="btn btn-primary">Ripristina</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+          </table>
+    </div>
+</section>
 @endsection
