b4572f1712763e72433cec0ef76bc041edbec663
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-22 22:39:30 +0100

    add fillable 'cover_image' at the class model 'Project'

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,7 +10,7 @@ class Project extends Model
 {
     use HasFactory;
     use SoftDeletes;
-    protected $fillable = ['title', 'slug', 'author', 'content', 'post_date'];
+    protected $fillable = ['title', 'slug', 'author', 'cover_image', 'content', 'post_date'];
 
     /**
     * Get the route key for the model.
