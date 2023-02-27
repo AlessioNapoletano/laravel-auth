65933f4fba2fd3a1312c214bbe7e8e67c86e7693
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-27 23:32:12 +0100

    Delete @include 'error' page into 'create' and 'edit' pages

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -6,7 +6,6 @@
 
 <section class="create-project">
     <div class="container py-5 mb-5">
-        @include('admin.projects.partials.error')
         @include('admin.projects.partials.form', ['method' => 'POST', 'action' => 'admin.projects.store', 'parameter' => '', 'buttonClass' => 'primary', 'buttonText' => 'Create project'])
     </div>
 </section>
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -6,7 +6,6 @@
 
 <section class="edit-project">
     <div class="container py-5 mb-5">
-        @include('admin.projects.partials.error')
         @include('admin.projects.partials.form', ['method' => 'PUT', 'action' => 'admin.projects.update', 'buttonClass' => 'success', 'buttonText' => 'Edit Project'])
     </div>
 </section>
